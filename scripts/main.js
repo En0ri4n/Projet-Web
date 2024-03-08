924aa9e3a297f1b3c4a159b119fb90a564e4d2fa
CedricHoog <cedric.hoog@gmail.com>
2024-03-08 13:20:52 +0100

    Added scroll back up button to tuteur_interne
    Le code pourrait être placé sur d'autres pages.
    Il faut juste mettre l'id premiere_section sur la 1ere zone visible pour utiliser le script

--- scripts/main.js
+++ scripts/main.js
@@ -42,3 +42,49 @@ const setCustomValidator = (input, regEx, message) =>
 		evt.target.reportValidity()
 	})
 }
+
+function checkVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+  }
+
+function scroll_up(){
+window.scrollTo({top, behavior:"smooth"});
+}
+
+
+var button_back_visible = false;
+var IdTimeout;
+/*Show the 'Scroll back to Top' button if the top of the page isn't visible*/
+window.addEventListener('scroll', function(event) 
+{
+	let button_back = document.getElementById('button_back');
+    if(checkVisible(document.getElementById("premiere_section")) && button_back_visible)
+    {
+		IdTimeout = window.setTimeout(function(){
+			button_back.style.display = 'none';
+		  },500);
+		button_back.animate(disappear,length_animation)
+		button_back_visible = false;
+    }
+	if (!checkVisible(document.getElementById("premiere_section")) && !button_back_visible ){
+		clearTimeout(IdTimeout);
+        button_back.style.display = 'flex';
+		button_back.animate(appear,length_animation)
+		button_back_visible = true;
+    }
+});
+const appear = [
+	{ opacity:0,},
+	{ opacity:1,},
+  ];
+  const disappear = [
+	{ opacity:1,},
+	{ opacity:0,},
+  ];
+  
+  const length_animation = {
+	duration: 500,
+	iterations: 1,
+  };
