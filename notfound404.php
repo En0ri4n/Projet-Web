1a9c1f4241b5b6657c8091d044af14983efd07fd
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-13 13:51:16 +0100

    Change error pages to php

--- notfound404.php
+++ notfound404.php
@@ -3,20 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <title>Interdiction</title>
-    <link rel="stylesheet" href="/styles/connexion.css">
+    <link rel="stylesheet" href="/assets/styles/connexion.css">
 </head>
 <body>
-<header class="header">
-    <img src="/assets/logo.png" alt="Logo">
-</header>
+<?php include 'components/header.php'; ?>
 <main style="display: flex; flex-direction: column">
     <h1>Erreur 404 - Page non trouvée</h1>
     <br>
     <img src="https://http.cat/images/404.jpg" style="width: max(30vw, 400px);" alt="notfound">
     <br>
     <p>Je crois que vous vous êtes perdu.</p>
-    <p><a href="/accueil.html">Retour à l'accueil</a></p>
+    <p><a href="/accueil.php">Retour à l'accueil</a></p>
 </main>
 <footer class="footer"></footer>
+<?php include 'components/footer.php'; ?>
 </body>
 </html>
