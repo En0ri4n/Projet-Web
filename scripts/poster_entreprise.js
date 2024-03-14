c2fa53374ad1ef3a898713834b99611bd349c5c9
hungrywwaffle <78042192+hungrywwaffle@users.noreply.github.com>
2024-03-14 09:24:54 +0100

    verification formulaire js

--- scripts/poster_entreprise.js
+++ scripts/poster_entreprise.js
@@ -1,6 +1,6 @@
 function check_data(){
     setCustomValidator(document.getElementById('nom_entreprise'), /^[a-zA-Z\s._-]{3,}$/, 'Le nom de lentreprise doit au moins contenir 3 caractères');
-    setCustomValidator(document.getElementById('siret'), /^[0-9\s.]{9}$/, 'Le numero de Siret doit contenir 9 chiffres'); /*TODO */
+    setCustomValidator(document.getElementById('siret'), /^[0-9\s.]{9}$/, 'Le numero de Siret doit contenir 9 chiffres'); 
     setCustomValidator(document.getElementById('domaine_tuteur'), /^[a-zA-Z\s._-]{3,}$/, 'Le nom du domaine doit au moins contenir 3 caractères');
     setCustomValidator(document.getElementById('nom_entreprise'), /^[a-zA-Z0-9\s._-]{3,}$/, 'Le nom de lentreprise doit au moins contenir 3 caractères');
     setCustomValidator(document.getElementById('description_entreprise_tuteur'), /^[a-zA-Z\s._-]{50,}$/, 'La description de lentreprise doit au moins contenir 3 caractères');
