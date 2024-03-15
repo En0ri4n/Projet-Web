f4177ece0e44dca6969b5959c2048f1691c73816
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-15 11:05:20 +0100

    Update sql queries (related to ad371ca14e0036986b6c789be7fc5510e5db2e96)

--- bdd/Stat_Etudiant.sql
+++ bdd/Stat_Etudiant.sql
@@ -1,20 +1,22 @@
 -- Active: 1710404343123@@cesi-project-web.sitam.me@3306@projet-web-eno
 SELECT Nom, Prenom
-FROM Utilisateur 
-WHERE IdUtilisateur 
-IN (SELECT IdEtudiant FROM Utilisateur); #nom de l'etudiant
+FROM Utilisateur
+WHERE IdUtilisateur
+          IN (SELECT IdUtilisateur FROM Etudiant);
+#nom de l'etudiant
 #prenom de l'etudiant
-SELECT  NomPromotion 
-FROM Promotion 
-INNER JOIN Etudiant ON Appartenir.IdPromotion = Appartenir.IdUtilisateur; #promo de l'etudiant
-SELECT Centre 
-FROM Promotion 
-INNER JOIN Etudiant ON Appartenir.IdPromotion = Appartenir.IdUtilisateur; #centre ou etudie l'etudiant
+SELECT NomPromotion
+FROM Promotion
+         INNER JOIN Etudiant ON Etudiant.IdPromotion = Promotion.IdUtilisateur; #promo de l'etudiant
+SELECT Centre
+FROM Promotion
+         INNER JOIN Etudiant ON Promotion.IdPromotion = Etudiant.IdPromotion;
+#centre ou etudie l'etudiant
 #nbre d'offres en wishlist
 #nbre d'offres postulees
 /*Cherche les données d'un etudiant basé sur son Id*/
 SELECT Utilisateur.Nom, Utilisateur.Prenom, Promotion.NomPromotion, Promotion.Centre
 FROM Utilisateur
-INNER JOIN Etudiant ON Utilisateur.IdUtilisateur = Etudiant.IdUtilisateur
-INNER JOIN Promotion ON Etudiant.IdPromotion = Promotion.IdPromotion
+         INNER JOIN Etudiant ON Utilisateur.IdUtilisateur = Etudiant.IdUtilisateur
+         INNER JOIN Promotion ON Etudiant.IdPromotion = Promotion.IdPromotion
 WHERE Etudiant.IdUtilisateur = 0;
