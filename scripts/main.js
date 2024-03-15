7436afb73d7b9f30c01f2261366c55ff0b92b83f
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-15 16:56:12 +0100

    Clean up

--- scripts/main.js
+++ scripts/main.js
@@ -24,30 +24,30 @@ export const addEventsTo = (elem, events) => events.forEach((event, callback) =>
  */
 export const setCustomValidator = (input, regEx, message) =>
 {
-	addEventTo(input, 'input', (evt) =>
-	{
-		const value = input.value;
+    addEventTo(input, 'input', (evt) =>
+    {
+        const value = input.value;
 
-		if (value.length === 0)
-		{
-			evt.target.className = ''
-			evt.target.setCustomValidity("")
-			return
-		}
+        if(value.length === 0)
+        {
+            evt.target.className = ''
+            evt.target.setCustomValidity("")
+            return
+        }
 
-		if (regEx.test(value))
-		{
-			evt.target.className = 'valid'
-			evt.target.setCustomValidity("")
-		}
-		else
-		{
-			evt.target.className = 'invalid'
-			evt.target.setCustomValidity(message);
-		}
+        if(regEx.test(value))
+        {
+            evt.target.className = 'valid'
+            evt.target.setCustomValidity("")
+        }
+        else
+        {
+            evt.target.className = 'invalid'
+            evt.target.setCustomValidity(message);
+        }
 
-		evt.target.reportValidity()
-	})
+        evt.target.reportValidity()
+    })
 }
 
 /**
@@ -61,96 +61,99 @@ export const setCustomValidator = (input, regEx, message) =>
  */
 export const setCustomConditionValidator = (input, condition, message) =>
 {
-	addEventTo(input, 'input', (evt) =>
-	{
-		const value = input.value;
+    addEventTo(input, 'input', (evt) =>
+    {
+        const value = input.value;
 
-		if (value.length === 0)
-		{
-			evt.target.className = ''
-			evt.target.setCustomValidity("")
-			return
-		}
+        if(value.length === 0)
+        {
+            evt.target.className = ''
+            evt.target.setCustomValidity("")
+            return
+        }
 
-		if (condition(value))
-		{
-			evt.target.className = 'valid'
-			evt.target.setCustomValidity("")
-		}
-		else
-		{
-			evt.target.className = 'invalid'
-			evt.target.setCustomValidity(message);
-		}
-		
-		evt.target.reportValidity()
-	})
+        if(condition(value))
+        {
+            evt.target.className = 'valid'
+            evt.target.setCustomValidity("")
+        }
+        else
+        {
+            evt.target.className = 'invalid'
+            evt.target.setCustomValidity(message);
+        }
+
+        evt.target.reportValidity()
+    })
 }
 
-export const sha256 = async message => {
-	const msgBuffer = new TextEncoder().encode(message);
-	const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-	const hashArray = Array.from(new Uint8Array(hashBuffer));
-	return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+export const sha256 = async message =>
+{
+    const msgBuffer = new TextEncoder().encode(message);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 };
 addEventTo(document, 'DOMContentLoaded', () =>
 {
-	const searchBar = document.getElementById('search-input');
+    const searchBar = document.getElementById('search-input');
 
-	if(searchBar == null)
-		return;
+    if(searchBar == null)
+        return;
 
-	addEventTo(searchBar, 'keyup', (evt) => {
-		if(evt.keyCode === 13)
-		{
-			console.log('Enter pressed');
-			window.location.href = `/search/${searchBar.value}`;
-		}
-	});
+    addEventTo(searchBar, 'keyup', (evt) =>
+    {
+        if(evt.keyCode === 13)
+        {
+            console.log('Enter pressed');
+            window.location.href = `/search/${searchBar.value}`;
+        }
+    });
 })
 
-function checkVisible(elm) {
+function checkVisible(elm)
+{
     var rect = elm.getBoundingClientRect();
     var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
-  }
-
-function scroll_up(){
-window.scrollTo({top, behavior:"smooth"});
 }
-
 
 var button_back_visible = false;
 var IdTimeout;
 /*Show the 'Scroll back to Top' button if the top of the page isn't visible*/
 window.addEventListener('scroll', function(event)
 {
-	let button_back = document.getElementById('button_back');
+    if(document.getElementById("premiere_section") == null)
+        return;
+
+    let button_back = document.getElementById('button_back');
     if(checkVisible(document.getElementById("premiere_section")) && button_back_visible)
     {
-		IdTimeout = window.setTimeout(function(){
-			button_back.style.display = 'none';
-		  },500);
-		button_back.animate(disappear,length_animation)
-		button_back_visible = false;
+        IdTimeout = window.setTimeout(function()
+                                      {
+                                          button_back.style.display = 'none';
+                                      }, 500);
+        button_back.animate(disappear, length_animation)
+        button_back_visible = false;
     }
-	if (!checkVisible(document.getElementById("premiere_section")) && !button_back_visible ){
-		clearTimeout(IdTimeout);
+    if(!checkVisible(document.getElementById("premiere_section")) && !button_back_visible)
+    {
+        clearTimeout(IdTimeout);
         button_back.style.display = 'flex';
-		button_back.animate(appear,length_animation)
-		button_back_visible = true;
+        button_back.animate(appear, length_animation)
+        button_back_visible = true;
     }
 });
 const appear = [
-	{ opacity:0,},
-	{ opacity:1,},
-  ];
-  const disappear = [
-	{ opacity:1,},
-	{ opacity:0,},
-  ];
-  
-  const length_animation = {
-	duration: 500,
-	iterations: 1,
-  };
+    {opacity: 0,},
+    {opacity: 1,},
+];
+const disappear = [
+    {opacity: 1,},
+    {opacity: 0,},
+];
+
+const length_animation = {
+    duration: 500,
+    iterations: 1,
+};
