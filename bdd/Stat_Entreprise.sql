f112810bc17e093c8ba47b7fd7e0ac3268a52c3b
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-15 11:41:35 +0100

    Fix queries

--- bdd/Stat_Entreprise.sql
+++ bdd/Stat_Entreprise.sql
@@ -1,29 +1,34 @@
 -- Active: 1710404343123@@cesi-project-web.sitam.me@3306@projet-web-eno
-SELECT NomEntreprise 
-FROM Entreprise 
-INNER JOIN Secteur ON Secteur.IdSecteur = Composer.IdSecteur 
-INNER JOIN Entreprise ON Entreprise.IdEntreprise = Composer.IdEntreprise
-WHERE Secteur.NomSecteur=''; #trier les entreprises par secteur
-SELECT NomEntreprise 
-FROM Entreprise 
-INNER JOIN Adresse ON Adresse.IdAdresse = Localiser.IdAdresse 
-INNER JOIN Entreprise ON Entreprise.IdEntreprise = Localiser.IdEntreprise
+SELECT NomEntreprise
+FROM Secteur
+         INNER JOIN Composer ON Secteur.IdSecteur = Composer.IdSecteur
+         INNER JOIN Entreprise ON Entreprise.IdEntreprise = Composer.IdEntreprise
+WHERE Secteur.NomSecteur = ''; #trier les entreprises par secteur
+
+SELECT NomEntreprise
+FROM Entreprise
+         INNER JOIN Localiser ON Entreprise.IdEntreprise = Localiser.IdEntreprise
+         INNER JOIN Adresse ON Adresse.IdAdresse = Localiser.IdAdresse
 WHERE Adresse.Ville = ''; #trier les entreprises par ville
+
 SELECT AVG(Note)
-FROM Evaluation 
-INNER JOIN Entreprise ON Evaluation.IdEvaluation = Entreprise.IdEntreprise 
-GROUP BY NomEntreprise
-WHERE IdEntreprise = '';#moyenne evaluation entreprise 
+FROM Evaluation
+         INNER JOIN Entreprise ON Evaluation.IdEvaluation = Entreprise.IdEntreprise
+WHERE Entreprise.IdEntreprise = 0
+GROUP BY NomEntreprise;
+
+#moyenne evaluation entreprise
 /*Cherche Entreprise via Secteur*/
-SELECT NomEntreprise 
+SELECT NomEntreprise
 FROM Entreprise
-INNER JOIN Composer ON Entreprise.IdEntreprise = Composer.IdEntreprise
-INNER JOIN Secteur ON Composer.IdSecteur = Secteur.IdSecteur 
-WHERE Secteur.NomSecteur='';
+         INNER JOIN Composer ON Entreprise.IdEntreprise = Composer.IdEntreprise
+         INNER JOIN Secteur ON Composer.IdSecteur = Secteur.IdSecteur
+WHERE Secteur.NomSecteur = '';
+
 /*Cherche Entreprise via Ville*/
-SELECT NomEntreprise 
+SELECT NomEntreprise
 FROM Entreprise
-INNER JOIN Localiser ON Entreprise.IdEntreprise = Localiser.IdEntreprise 
-INNER JOIN Adresse ON Localiser.IdAdresse = Adresse.IdAdresse 
+         INNER JOIN Localiser ON Entreprise.IdEntreprise = Localiser.IdEntreprise
+         INNER JOIN Adresse ON Localiser.IdAdresse = Adresse.IdAdresse
 WHERE Ville = '';
 
