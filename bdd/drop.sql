996a03fe1c250e7a613b93fcf6b627b523e11af5
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-15 11:32:05 +0100

    Add drop script

---
+++ bdd/drop.sql
@@ -0,0 +1,36 @@
+drop table AdminWishlist;
+
+alter table Administrateur
+    drop foreign key Administrateur_ibfk_2;
+
+drop table Candidature;
+
+drop table Composer;
+
+drop table Demander;
+
+drop table Competence;
+
+drop table Evaluation;
+
+drop table Localiser;
+
+drop table Wishlist;
+
+drop table Etudiant;
+
+drop table Offre;
+
+drop table Adresse;
+
+drop table Entreprise;
+
+drop table Promotion;
+
+drop table Administrateur;
+
+drop table Pilote;
+
+drop table Secteur;
+
+drop table Utilisateur;
