c42fedad0835be3dfce4b83a94e5ee5ad9d80597
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-18 15:26:10 +0100

    Use of helpers of a71b4688ba894ef14919c1d290b86962fbd7fd2d

---
+++ api/login.php
@@ -0,0 +1,48 @@
+<?php
+global $pdo;
+require_once 'config.php';
+include_once 'table/UtilisateurTable.php';
+
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Handle HTTP methods
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method)
+{
+    case 'POST':
+        $data = json_decode(file_get_contents('php://input'), true);
+        $username = $data['username'];
+        $password = $data['password'];
+
+        try
+        {
+            $utilisateur = UtilisateurTable::getUtilisateurTable($pdo)->selectUtilisateur(array(), array(UtilisateurTable::$ID_COLUMN => $username, UtilisateurTable::$PASSWORD_COLUMN => $password));
+
+            if(!$utilisateur->isEmpty())
+            {
+                setcookie('userId', $utilisateur->getId(), time() + 3600, '/');
+                echo json_encode(['authorized' => true, 'message' => 'User authentified', 'user' => $utilisateur->getId(), 'redirect' => '/accueil.php']);
+            }
+            else
+            {
+                echo json_encode(['authorized' => false, 'message' => 'Invalid username or password']);
+            }
+            return;
+
+        }
+        catch(Exception $e)
+        {
+            http_response_code(500);
+            echo json_encode(['error' => $e->getMessage()]);
+        }
+        break;
+    case 'PUT':
+    case 'DELETE':
+    case 'GET':
+    default:
+        // Invalid method
+        http_response_code(405);
+        echo json_encode(['error' => 'Method not allowed']);
+        break;
+}
