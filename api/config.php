b3d8392b245ffcb50eb5c4b27d5d18ce3c526915
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-19 19:57:29 +0100

    Add global variables for default page, cookie name and connection page

--- api/config.php
+++ api/config.php
@@ -3,6 +3,11 @@ $HOST = 'cesi-project-web.sitam.me:3306';
 $DB_NAME = 'projet-web-eno';
 $USERNAME = 'enow';
 $PASSWORD = 'enow';
+
+$USER_COOKIE_NAME = 'user';
+$DEFAULT_PAGE = '/accueil.php';
+$CONNECTION_PAGE = '/connexion.php';
+
 try
 {
     $pdo = new PDO("mysql:host=$HOST;dbname=$DB_NAME", $USERNAME, $PASSWORD);
