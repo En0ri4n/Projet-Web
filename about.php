63be84cf56b134aeea67ae9c5b2906f6f1c360bf
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-19 20:25:39 +0100

    Add team picture to about page

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
     <p class="paragraphe">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet consectetur adipiscing elit. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu. Urna nunc id cursus metus. Accumsan sit amet nulla facilisi morbi tempus iaculis urna. Lorem donec massa sapien faucibus. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Diam sollicitudin tempor id eu nisl nunc mi.
     </p>
-    <img id="valowont" src="/assets/IMG_valowont.png" alt="Notre équipe">
+    <img id="valowont" src="/assets/team_valowont.jpg" alt="Notre équipe">
 
 </main>
 <?php require_once('components/footer.php'); ?>
