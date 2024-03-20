9bf275cafe3dc80d461afbc8ae15c6731082eef8
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-20 11:20:30 +0100

    Add clear script

---
+++ bdd/clear.sql
@@ -0,0 +1,33 @@
+DELETE FROM AdminWishlist;
+
+DELETE FROM Candidature;
+
+DELETE FROM Composer;
+
+DELETE FROM Demander;
+
+DELETE FROM Competence;
+
+DELETE FROM Evaluation;
+
+DELETE FROM Localiser;
+
+DELETE FROM Wishlist;
+
+DELETE FROM Etudiant;
+
+DELETE FROM Offre;
+
+DELETE FROM Adresse;
+
+DELETE FROM Entreprise;
+
+DELETE FROM Promotion;
+
+DELETE FROM Administrateur;
+
+DELETE FROM Pilote;
+
+DELETE FROM Secteur;
+
+DELETE FROM Utilisateur;
