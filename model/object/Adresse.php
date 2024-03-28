cd250401f8e0ff69fc8a689642d60f98e2148ad6
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-28 12:19:13 +0100

    Add Adresse Object and Table

---
+++ model/object/Adresse.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/object/SerializableInterface.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/object/Adresse.php');
+
+class Adresse implements SerializableInterface
+{
+    private int $id;
+    private int $numero;
+    private string $rue;
+    private string $ville;
+    private string $codePostal;
+    private string $pays;
+
+    public function __construct(int $id, int $numero, string $rue, string $ville, string $codePostal, string $pays)
+    {
+        $this->id = $id;
+        $this->numero = $numero;
+        $this->rue = $rue;
+        $this->ville = $ville;
+        $this->codePostal = $codePostal;
+        $this->pays = $pays;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNumero(): int
+    {
+        return $this->numero;
+    }
+
+    public function getRue(): string
+    {
+        return $this->rue;
+    }
+
+    public function getVille(): string
+    {
+        return $this->ville;
+    }
+
+    public function getCodePostal(): string
+    {
+        return $this->codePostal;
+    }
+
+    public function getPays(): string
+    {
+        return $this->pays;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            explode('.', AdresseTable::$ID_COLUMN)[1] => $this->id,
+            explode('.', AdresseTable::$NUMERO_COLUMN)[1] => $this->numero,
+            explode('.', AdresseTable::$RUE_COLUMN)[1] => $this->rue,
+            explode('.', AdresseTable::$VILLE_COLUMN)[1] => $this->ville,
+            explode('.', AdresseTable::$CODE_POSTAL_COLUMN)[1] => $this->codePostal,
+            explode('.', AdresseTable::$PAYS_COLUMN)[1] => $this->pays
+        ];
+    }
+
+    public static function fromArray(array $array): Adresse
+    {
+        return new Adresse(
+            $array[explode('.', AdresseTable::$ID_COLUMN)[1]],
+            $array[explode('.', AdresseTable::$NUMERO_COLUMN)[1]],
+            $array[explode('.', AdresseTable::$RUE_COLUMN)[1]],
+            $array[explode('.', AdresseTable::$VILLE_COLUMN)[1]],
+            $array[explode('.', AdresseTable::$CODE_POSTAL_COLUMN)[1]],
+            $array[explode('.', AdresseTable::$PAYS_COLUMN)[1]]);
+    }
+}
---
+++ model/table/AdresseTable.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/object/Adresse.php');
+
+class AdresseTable extends AbstractTable
+{
+    public static string $ID_COLUMN = 'Adresse.IdAdresse';
+    public static string $NUMERO_COLUMN = 'Adresse.Numero';
+    public static string $RUE_COLUMN = 'Adresse.Rue';
+    public static string $VILLE_COLUMN = 'Adresse.Ville';
+    public static string $CODE_POSTAL_COLUMN = 'Adresse.CodePostal';
+    public static string $PAYS_COLUMN = 'Adresse.Pays';
+
+    public function insert(SerializableInterface $obj): bool
+    {
+        return $this->defaultInsert($obj);
+    }
+
+    public function select(array $conditions): null|Adresse|array
+    {
+        return $this->defaultSelect($conditions);
+    }
+
+    public function update(mixed $id, array $columns, array $values): bool
+    {
+        return $this->defaultUpdate($id, $columns, $values);
+    }
+
+    public function delete(mixed $id): bool
+    {
+        return $this->defaultDelete($id);
+    }
+
+    protected function getIdColumn(): string
+    {
+        return self::$ID_COLUMN;
+    }
+}
