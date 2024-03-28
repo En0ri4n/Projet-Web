35d9e1558b63b25c284753c19ce67c79f09354b0
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-28 11:43:04 +0100

    Update

--- model/table/AdministrateurTable.php
+++ model/table/AdministrateurTable.php
@@ -1,48 +1,83 @@
 <?php
 
-namespace table;
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/table/AbstractTable.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/object/Administrateur.php');
 
 class AdministrateurTable extends AbstractTable
 {
-    public static string $ID_COLUMN = "Utilisateur.IdUtilisateur";
+    public static string $ID_COLUMN = "Administrateur.IdUtilisateur";
     public static string $NOM_COLUMN = "Utilisateur.Nom";
     public static string $PRENOM_COLUMN = "Utilisateur.Prenom";
     public static string $EMAIL_COLUMN = "Utilisateur.MailUtilisateur";
     public static string $PASSWORD_COLUMN = "Utilisateur.MotDePasse";
     public static string $TELEPHONE_COLUMN = "Utilisateur.TelephoneUtilisateur";
 
-    public function __construct($db)
+    public function __construct()
     {
-        parent::__construct('Administrateur', $db);
+        parent::__construct('Administrateur');
     }
 
     public function insert(mixed $obj): bool
     {
         // TODO: Implement insert() method.
+        return false;
     }
 
-    public function select(array $conditions): mixed
+    public function select(array $conditions): null|array|Administrateur
     {
-        // TODO: Implement select() method.
+        try
+        {
+            $query = "SELECT * FROM " . $this->getTableName() . " INNER JOIN Utilisateur ON Utilisateur.IdUtilisateur = Administrateur.IdUtilisateur";
+
+            if(empty($conditions))
+            {
+                $stmt = $this->getDatabase()->query($query);
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                return array_map((fn($row) => Etudiant::fromArray($row)), $rows);
+            }
+
+            $query .= " WHERE " . implode(" AND ", array_map((fn($key) => $key . " = :" . $this->escape_and_lower($key)), array_keys($conditions)));
+
+            $stmt = $this->getDatabase()->prepare($query);
+            foreach($conditions as $key => $value)
+                $stmt->bindValue(':' . $this->escape_and_lower($key), $value);
+            $stmt->execute();
+
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if(count($rows) == 1)
+                return Administrateur::fromArray($rows[0]);
+            else if(count($rows) > 1)
+                return array_map((fn($row) => Administrateur::fromArray($row)), $rows);
+        }
+        catch(Exception $e)
+        {
+        }
+        return null;
     }
 
     public function update(mixed $id, array $columns, array $values): bool
     {
         // TODO: Implement update() method.
+        return false;
     }
 
     public function delete(mixed $id): bool
     {
         // TODO: Implement delete() method.
+        return false;
     }
 
-    protected function getIdColumn(): string
+    public function getIdColumn(): string
     {
-        // TODO: Implement getIdColumn() method.
+        return self::$ID_COLUMN;
     }
 
-    protected function getColumnCount(): int
+    public static function isAdministrateur(mixed $userId): bool
     {
-        // TODO: Implement getColumnCount() method.
+        $table = new AdministrateurTable();
+        $administrateur = $table->select([self::$ID_COLUMN => $userId]);
+        return $administrateur != null;
     }
 }
--- model/table/PiloteTable.php
+++ model/table/PiloteTable.php
@@ -1,23 +1,26 @@
 <?php
 
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/table/UtilisateurTable.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/object/Pilote.php');
 
-namespace table;
-
-use object\Pilote;
-
-class PiloteTable extends UtilisateurTable
+class PiloteTable extends AbstractTable
 {
+    public static string $ID_COLUMN = "Pilote.IdUtilisateur";
+
+    public function __construct() { parent::__construct('Pilote'); }
+
     /**
      * Create a new pilote in the database
      *
-     * @param Pilote $pilote The pilote to create
+     * @param Pilote $obj The pilote to create
      * @return bool True if the pilote was created, false otherwise
      * @throws Exception If the query fails or parameters are invalid
      */
-    public function insertPilote(Pilote $pilote): bool
+    public function insert(mixed $obj): bool
     {
-        $created = self::getUtilisateurTable()->insertUtilisateur($pilote);
-        $created &= $this->insert([self::$ID_COLUMN], [$pilote->getId()]);
+        $utilisateur_table = new UtilisateurTable();
+        $created = $utilisateur_table->insert($obj);
+        $created &= $this->insert([self::$ID_COLUMN => $obj->getId()]);
         return $created;
     }
 
@@ -25,31 +28,64 @@ class PiloteTable extends UtilisateurTable
      * Select a pilote from the database
      *
      * @param array $conditions Associative array of column keys and values to select
-     * @return Pilote|null The pilote selected or an empty pilote if the query fails
+     * @return array|Pilote|null The pilote selected or an empty pilote if the query fails
      */
-    public function selectPilote(array $conditions): Pilote|null
+    public function select(array $conditions): array|Pilote|null
     {
         try
         {
-            $stmt = $this->select($conditions);
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-            return $row ? Pilote::fromArray($row) : null;
+            $query = "SELECT * FROM " . $this->getTableName() . " INNER JOIN Utilisateur ON Utilisateur.IdUtilisateur = Pilote.IdUtilisateur";
+
+            if(empty($conditions))
+            {
+                $stmt = $this->getDatabase()->query($query);
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                return array_map((fn($row) => Etudiant::fromArray($row)), $rows);
+            }
+
+            $query .= " WHERE " . implode(" AND ", array_map((fn($key) => $key . " = :" . $this->escape_and_lower($key)), array_keys($conditions)));
+
+            $stmt = $this->getDatabase()->prepare($query);
+            foreach($conditions as $key => $value)
+                $stmt->bindValue(':' . $this->escape_and_lower($key), $value);
+            $stmt->execute();
+
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if(count($rows) == 1)
+                return Pilote::fromArray($rows[0]);
+            else if(count($rows) > 1)
+                return array_map((fn($row) => Pilote::fromArray($row)), $rows);
         }
         catch(Exception $e)
         {
-            return null;
         }
+        return null;
     }
 
-    /**
-     * Get the pilote table
-     *
-     * @return PiloteTable The pilote table
-     */
-    public static function getPiloteTable(): PiloteTable
+    public function update(mixed $id, array $columns, array $values): bool
     {
-        $instance = new PiloteTable();
-        $instance->setTable("Pilote");
-        return $instance;
+        // TODO: Implement update() method.
+        return false;
+    }
+
+    public function delete(mixed $id): bool
+    {
+        // TODO: Implement delete() method.
+        return false;
+    }
+
+    public function getIdColumn(): string
+    {
+        // TODO: Implement getIdColumn() method.
+        return self::$ID_COLUMN;
+    }
+
+    public static function isPilote(mixed $userId): bool
+    {
+        $table = new PiloteTable();
+        $pilote = $table->select([self::$ID_COLUMN => $userId]);
+        return $pilote !== null;
     }
 }
