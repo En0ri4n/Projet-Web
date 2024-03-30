f34578518709023c678165039f22d275562af465
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-03-30 11:31:09 +0100

    Add connection check function for requests

--- api/requests.php
+++ api/requests.php
@@ -1,6 +1,17 @@
 <?php
 
-function url_decode_and_percent(string $s)
+require_once($_SERVER['DOCUMENT_ROOT'] . '/controller/Controller.php');
+
+function check_connection(): void
+{
+    if(!isset($_COOKIE[Controller::$USER_COOKIE_NAME]))
+    {
+        http_response_code(401);
+        echo json_encode(['error' => 'Vous devez être connecté pour effectuer cette action']);
+        exit();
+    }
+}
+function url_decode_and_percent(string $s): string
 {
     return '%' . urldecode($s) . '%';
 }
