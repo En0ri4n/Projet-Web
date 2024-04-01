e64cb7ca386dbffe683aba9b7cbdce4cb84b229a
Enorian <enorian.rajo@gmail.com>
2024-04-02 00:55:48 +0200

    Add non-existent type attribute (?!?)

--- model/object/Promotion.php
+++ model/object/Promotion.php
@@ -8,16 +8,18 @@ class Promotion extends SerializableObject
 {
     private int $id_promotion;
     private string $nom;
+    private string $type;
     private string $date_debut;
     private int $niveau;
     private int $duree;
     private string $centre;
     private string $id_pilote;
 
-    public function __construct(int $id_promotion, string $nom, string $date_debut, int $niveau, int $duree, string $centre, string $pilote)
+    public function __construct(int $id_promotion, string $nom, string $type, string $date_debut, int $niveau, int $duree, string $centre, string $pilote)
     {
         $this->id_promotion = $id_promotion;
         $this->nom = $nom;
+        $this->type = $type;
         $this->date_debut = $date_debut;
         $this->niveau = $niveau;
         $this->duree = $duree;
@@ -33,6 +35,11 @@ class Promotion extends SerializableObject
     public function getNom(): string
     {
         return $this->nom;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
     }
 
     public function getDateDebut(): string
@@ -65,6 +72,7 @@ class Promotion extends SerializableObject
         return [
             self::getColumnName(PromotionTable::$ID_COLUMN) => $this->id_promotion,
             self::getColumnName(PromotionTable::$NAME_COLUMN) => $this->nom,
+            self::getColumnName(PromotionTable::$TYPE_COLUMN) => $this->type,
             self::getColumnName(PromotionTable::$DATE_COLUMN) => $this->date_debut,
             self::getColumnName(PromotionTable::$LEVEL_COLUMN) => $this->niveau,
             self::getColumnName(PromotionTable::$DURATION_COLUMN) => $this->duree,
@@ -78,6 +86,7 @@ class Promotion extends SerializableObject
         return new Promotion(
             $array[self::getColumnName(PromotionTable::$ID_COLUMN)],
             $array[self::getColumnName(PromotionTable::$NAME_COLUMN)],
+            $array[self::getColumnName(PromotionTable::$TYPE_COLUMN)],
             $array[self::getColumnName(PromotionTable::$DATE_COLUMN)],
             $array[self::getColumnName(PromotionTable::$LEVEL_COLUMN)],
             $array[self::getColumnName(PromotionTable::$DURATION_COLUMN)],
