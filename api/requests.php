02fd7e9fa92188fbfc41fcb24df90e7760311649
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-04-02 15:06:14 +0200

    Add utility function for requests to check if current user is admin
    Took 2 minutes

--- api/requests.php
+++ api/requests.php
@@ -14,12 +14,24 @@ function addIfSetLike(array &$array, array $get_array, string $key, string $colu
         $array[$column] = url_decode_and_percent($get_array[$key]);
 }
 
-function check_connection(): void
+function checkConnection(): void
 {
     if(!isset($_COOKIE[Controller::$USER_COOKIE_NAME]))
     {
         http_response_code(401);
         echo json_encode(['error' => 'Vous devez être connecté pour effectuer cette action']);
+        exit();
+    }
+}
+
+function checkAdminConnection()
+{
+    checkConnection();
+
+    if(!AdministrateurTable::isAdministrateur(Controller::getCurrentUser()->getId()))
+    {
+        http_response_code(403);
+        echo json_encode(['error' => 'Vous devez être administrateur pour effectuer cette action']);
         exit();
     }
 }
