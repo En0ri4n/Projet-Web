e1d06c2751be27ddc5e499f354a014e6178c0211
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2024-04-02 18:25:21 +0200

    Remove TODO
    Took 8 seconds

--- api/logout.php
+++ api/logout.php
@@ -2,9 +2,6 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/controller/Controller.php');
 
-/**
- * TODO: Handle the disconnection of the user. delete the session and redirect to the home page.
- */
 if(isset($_COOKIE[Controller::$USER_COOKIE_NAME]))
 {
     unset($_COOKIE[Controller::$USER_COOKIE_NAME]);
