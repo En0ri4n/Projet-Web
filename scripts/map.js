5ed6bb99f36cff65cfd9e126588925d010835aa0
hungrywwaffle <78042192+hungrywwaffle@users.noreply.github.com>
2024-04-03 10:58:34 +0200

    update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,4 @@
+/*set map*/
 var map = L.map('map');
     map.setView([51.505, -0.09], 13);
 
@@ -6,7 +7,15 @@ var map = L.map('map');
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+/*geoapify api key*/
 let apiKey = "8b0b65847e964e828fb065d72b3a57b4";
+
+/*get id from url*/
+url = new URL(window.location.href);
+let id = url.searchParams.get("entrepriseId");
+
+var req = new XMLHttpRequest();
+req.open('GET', '/api/entreprises?IdEntreprise=1/adresses');
 
 const address = 'Baldersgade 3B, 2200 Copenhagen, Denmark';
 
@@ -16,11 +25,11 @@ fetch(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(addr
 	console.log(geocodingResult);
 });
 
-addressurl = "https://api.geoapify.com/v1/geocode/search?housenumber=11&street=Rue%20Grenette&postcode=69002&city=Lyon&country=France&apiKey=8b0b65847e964e828fb065d72b3a57b4";
+/*addressurl = "https://api.geoapify.com/v1/geocode/search?housenumber=11&street=Rue%20Grenette&postcode=69002&city=Lyon&country=France&apiKey=8b0b65847e964e828fb065d72b3a57b4";
 
 navigator.geolocation.watchPosition(success, error);*/
 
-let marker;
+/*let marker;
 
 marker = L.marker([lat, lon]).addTo(map);
-map.setView([address.lat, address.lon], 17);
+map.setView([address.lat, address.lon], 17);*/
